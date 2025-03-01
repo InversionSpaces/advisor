07a5718d776b631eb070a1e7f28ed715542f7bce
InversionSpaces <inversionspaces@vivaldi.net>
2025-03-01 15:06:46 +0100

    Remove everything

--- backend/api/db.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-from django.conf import settings
-
-# Initialize the database client
-client = MongoClient(settings.MONGODB_SETTINGS['host'], settings.MONGODB_SETTINGS['port'])
-db = client[settings.MONGODB_SETTINGS['db']]
--- frontend/src/config.js
+++
@@ -1,3 +0,0 @@
-// Configuration file for global settings
-
-export const BACKEND_URL = 'http://localhost:8000'; // Replace with your actual backend URL 
